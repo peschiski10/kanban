4bb2dc59e1534a93d4363eccedb296e204f404f2
peschiski10 <julianopeschiski@gmail.com>
2023-04-20 09:43:11 -0300

    first commit

---
+++ kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-11c92',
+    appId: '1:78536285740:web:5d7fe77abbdf972e12bb89',
+    storageBucket: 'kanbanfire-11c92.appspot.com',
+    apiKey: 'AIzaSyCTmdHIAW5QRrVFQgHxToAxe5Zg1q5E31U',
+    authDomain: 'kanbanfire-11c92.firebaseapp.com',
+    messagingSenderId: '78536285740',
+    measurementId: 'G-VX9RP88VPT',
+  },};
---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-11c92',
+    appId: '1:78536285740:web:5d7fe77abbdf972e12bb89',
+    storageBucket: 'kanbanfire-11c92.appspot.com',
+    apiKey: 'AIzaSyCTmdHIAW5QRrVFQgHxToAxe5Zg1q5E31U',
+    authDomain: 'kanbanfire-11c92.firebaseapp.com',
+    messagingSenderId: '78536285740',
+    measurementId: 'G-VX9RP88VPT',
+  },};
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCTmdHIAW5QRrVFQgHxToAxe5Zg1q5E31U",
+    authDomain: "kanbanfire-11c92.firebaseapp.com",
+    projectId: "kanbanfire-11c92",
+    storageBucket: "kanbanfire-11c92.appspot.com",
+    messagingSenderId: "78536285740",
+    appId: "1:78536285740:web:5d7fe77abbdf972e12bb89",
+    measurementId: "G-VX9RP88VPT"
+  };
